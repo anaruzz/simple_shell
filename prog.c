fdabc30fe4a3054e697e13d04c8705bac1d5f76c
anaruzz <laabidigh@gmail.com>
2019-11-25 14:57:10 +0100

    Task 3 Simple shell 0.1

--- prog.c
+++ prog.c
@@ -5,9 +5,43 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #define BUFFER_LEN 1024
+
 /**
- *main - fn - int
- *Return: int
+ * tokenize - parse the input string
+ * @line: string
+ * @argvv: array of strings to fill
+ * @bufsize: size of buffer for allocation
+ *Return: void
+ */
+void tokenize(char *line, char **argvv, int bufsize)
+{
+	char *token;
+	int	i = 0;
+
+	token = strtok(line, " ");
+	while (token != NULL)
+	{
+		argvv[i] = token;
+		token = strtok(NULL, " ");
+		i++;
+		if (i >= bufsize)
+		{
+			bufsize += BUFFER_LEN;
+			argvv = realloc(argvv, bufsize * sizeof(char *));
+			if (!argvv)
+			{
+				fprintf(stderr, "shell: Allocation error!\n");
+				exit(EXIT_FAILURE);
+			}
+		}
+	}
+	argvv[i] = '\0';
+}
+
+/**
+ *main - main program for the shell project
+ *
+ *Return: 0
  */
 int main(void)
 {
@@ -15,61 +49,39 @@ int main(void)
 	char **argvv = malloc(sizeof(char) * bufsize);
 	char *token, *line;
 	size_t length = 0;
-	if (!argvv) 
+
+	if (!argvv)
 	{
 		fprintf(stderr, "lsh: allocation error\n");
 		exit(EXIT_FAILURE);
 	}
-	while(1)
+	while (1)
 	{
-		printf("shell$ ");
-		if((( read = getline(&line, &length , stdin)) < -1))
+		printf("$ ");
+		read = getline(&line, &length, stdin);
+		if (read < -1)
 			break;
 		length = strlen(line);
 		if (line[length - 1] == '\n')
 			line[length - 1] = '\0';
-		token = strtok(line," ");
-		i=0;
-		while(token!=NULL)
+
+		tokenize(line, argvv, bufsize);
+
+
+		pid = fork();
+		if (pid == 0)
 		{
-			argvv[i]= token;
-			token = strtok(NULL," ");
-			i++;
-			if (i >= bufsize)
-			{
-				bufsize += BUFFER_LEN;
-				argvv = realloc(argvv, bufsize * sizeof(char *));
-				if (!argvv)
-				{
-					fprintf(stderr, "shell: Allocation error!\n");
-					exit(EXIT_FAILURE);
-
-
-				}
-			}
-		}
-		argvv[i]='\0';
-		pid= fork();
-		if(pid==0){
-			execve(argvv[0], argvv, NULL);
+			if (execve(argvv[0], argvv, NULL) == -1)
 			fprintf(stderr, "Child process could not do execve\n");
-
 		}
 		else
 		{
 			wait(NULL);
-			printf("Child exited\n");
 		}
 
-		/*if(getcwd(*argvv,sizeof(argvv)) == NULL)*/ 				
+		/*if(getcwd(*argvv,sizeof(argvv)) == NULL)*/
 			/*perror("getcwd(): error\n");*/
 
 	}
 	return (0);
 }
-
-
-
-
-char *tok( 
-
