719718dc2d90790ca6dab798f7f6c9ad06c03634
yasmine <1055@holbertonschool.com>
2019-11-26 16:47:38 +0000

    commit

--- prog.c
+++ prog.c
@@ -6,38 +6,38 @@
 #include <sys/wait.h>                           
 #include <sys/stat.h>                           
 #define BUFFER_LEN 1024                         
-                                                
+
 /**                                             
-  * concat_all - concatenate two strings          
-  * @s1: string                                   
-  * @s2: string                                   
-  *Return: string                                 
-  */                                              
+ * concat_all - concatenate two strings          
+ * @s1: string                                   
+ * @s2: string                                   
+ *Return: string                                 
+ */                                              
 char *concat_all(char *s1, char *s2)            
 {                                               
 	char *result = NULL;                            
 	int l1 = 0, l2 = 0, i = 0, k = 0;               
-	                                                
+
 	for (l1 = 0; s1[l1]; l1++)                      
 		;                                               
-	                                                
+
 	for (l2 = 0; s2[l2]; l2++)                      
 		;                                               
-	                                                
+
 	result = malloc(sizeof(char) * (l1 + l2 + 1));  
 	if (!result)                                    
-		        return (NULL);                          
-	                                                
+		return (NULL);                          
+
 	for (i = 0; s1[i]; i++)                         
-		        result[i] = s1[i];                      
+		result[i] = s1[i];                      
 	k = i;                                          
-	                                                
+
 	for (i = 0; s2[i]; i++)                         
-		        result[k + i] = s2[i];                  
+		result[k + i] = s2[i];                  
 	k = k + i;                                      
-	                                                
+
 	result[k] = '\0';                               
-	                                                
+
 	return (result);                                
 }                                               
 
@@ -47,10 +47,10 @@ char *concat_all(char *s1, char *s2)
 
 
 /**
-  * find_path - find the path of the command
-  * @av: the command to look for its path
-  *Return: string: full path of the command
-  */
+ * find_path - find the path of the command
+ * @av: the command to look for its path
+ *Return: string: full path of the command
+ */
 char *find_path(char *av)
 {
 	char *path = NULL, *token = NULL, *cpath = NULL;
@@ -148,3 +148,107 @@ int check_builtin(char *argvv)
 	return (-1);                                                                 
 }                                                                            
 
+
+
+
+/**
+ * print_env - print all environment variables
+ *
+ *Return: Void
+ */
+void print_env(void)
+{
+	extern char **environ;
+	int i;
+	char *s = *environ;
+
+	for (i = 1; s != NULL; i++)
+	{
+		printf("%s\n", s);
+		s = environ[i];
+	}
+
+}
+
+/**
+ * change_dir - change directory with builtin cd
+ * argvv: array of string arguments
+ *Return: Void
+ */
+void change_dir (char **argvv)
+{
+	char *pth;
+	int i, j;
+	if (argvv[1] == NULL)
+		pth = getenv("HOME");
+	else if (strcmp(argvv[1], "-") == 0)
+	{
+		pth = getenv("PWD");
+		for (i = 0; pth[i]; i++)
+			;
+		for (j = i; pth[j] != '/'; j--)
+			;
+		if (pth[j] == '/')
+			pth[j] = '\0';
+	}
+	else
+		pth = argvv[1];
+	if (chdir(pth) == -1)
+		perror("cd: can't cd to directory");
+	setenv("PWD", pth, 1);
+}
+
+/* Function to execute builtin */
+/**
+ * builtins - execute builtins
+ * @argvv:array of strings to execute builtin
+ *Return: Void
+ */
+void builtins(char **argvv, int i)
+{
+
+	switch (i)
+	{
+		case 1:
+			exit(atoi(argvv[1]));
+			break;
+		case 2:
+			change_dir(argvv);
+			break;
+		case 3:
+			puts("shell: 1: help: not found");
+			break;
+		case 4:
+			print_env();
+			break;
+		default:
+			break;
+	}
+
+}
+
+/**
+ * execute - execute command
+ * @argvv: array of strings to check
+ *Return: void
+ */
+void execute(char **argvv)
+{
+	int pid = 0;
+	pid = fork();
+	if (pid == 0)
+	{
+		if (execve(argvv[0], argvv, NULL) == -1)
+		{
+			perror("hsh");
+			exit(0);
+		}
+	}
+	else if (pid == -1)
+	{
+		perror("hsh error - child is -1");
+		exit(-1);
+	}
+	else
+		wait(NULL);
+}
