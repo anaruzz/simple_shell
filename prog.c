c91c6eea012bfe515e8b1d9dc4f1d95a8fdea028
yasmine <1055@holbertonschool.com>
2019-11-26 16:42:55 +0000

    commit

--- prog.c
+++ prog.c
@@ -1,86 +1,150 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#define BUFFER_LEN 1024
+#include <stdio.h>                              
+#include <stdlib.h>                             
+#include <string.h>                             
+#include <unistd.h>                             
+#include <sys/types.h>                          
+#include <sys/wait.h>                           
+#include <sys/stat.h>                           
+#define BUFFER_LEN 1024                         
+                                                
+/**                                             
+  * concat_all - concatenate two strings          
+  * @s1: string                                   
+  * @s2: string                                   
+  *Return: string                                 
+  */                                              
+char *concat_all(char *s1, char *s2)            
+{                                               
+	char *result = NULL;                            
+	int l1 = 0, l2 = 0, i = 0, k = 0;               
+	                                                
+	for (l1 = 0; s1[l1]; l1++)                      
+		;                                               
+	                                                
+	for (l2 = 0; s2[l2]; l2++)                      
+		;                                               
+	                                                
+	result = malloc(sizeof(char) * (l1 + l2 + 1));  
+	if (!result)                                    
+		        return (NULL);                          
+	                                                
+	for (i = 0; s1[i]; i++)                         
+		        result[i] = s1[i];                      
+	k = i;                                          
+	                                                
+	for (i = 0; s2[i]; i++)                         
+		        result[k + i] = s2[i];                  
+	k = k + i;                                      
+	                                                
+	result[k] = '\0';                               
+	                                                
+	return (result);                                
+}                                               
+
+
+
+
+
 
 /**
- * tokenize - parse the input string
- * @line: string
- * @argvv: array of strings to fill
- * @bufsize: size of buffer for allocation
- *Return: void
- */
-void tokenize(char *line, char **argvv, int bufsize)
+  * find_path - find the path of the command
+  * @av: the command to look for its path
+  *Return: string: full path of the command
+  */
+char *find_path(char *av)
 {
-	char *token;
-	int	i = 0;
+	char *path = NULL, *token = NULL, *cpath = NULL;
+	int i = 0, len = 0;
+	struct stat sfile;
 
-	token = strtok(line, " ");
+	path = getenv("PATH");
+	for (len = 0; path[len]; len++)
+		;
+
+	cpath = malloc(sizeof(char) * len + 1);
+
+	for (i = 0; path[i]; i++)
+		cpath[i] = path[i];
+	cpath[i] = '\0';
+
+
+
+	token = strtok(cpath, ":");
+	token = concat_all(token, "/");
+	token = concat_all(token, av);
 	while (token != NULL)
 	{
-		argvv[i] = token;
-		token = strtok(NULL, " ");
-		i++;
-		if (i >= bufsize)
+		if (stat(token, &sfile) == 0)
+			return (token);
+
+		token = strtok(NULL, ":");
+		if (token != NULL)
 		{
-			bufsize += BUFFER_LEN;
-			argvv = realloc(argvv, bufsize * sizeof(char *));
-			if (!argvv)
-			{
-				fprintf(stderr, "shell: Allocation error!\n");
-				exit(EXIT_FAILURE);
-			}
+			token = concat_all(token, "/");
+			token = concat_all(token, av);
 		}
+
 	}
-	argvv[i] = '\0';
+
+	return (NULL);
 }
 
-/**
- *main - main program for the shell project
- *
- *Return: 0
- */
-int main(void)
-{
-	int bufsize = BUFFER_LEN, i, pid, read;
-	char **argvv = malloc(sizeof(char) * bufsize);
-	char *token, *line;
-	size_t length = 0;
-
-	if (!argvv)
-	{
-		fprintf(stderr, "lsh: allocation error\n");
-		exit(EXIT_FAILURE);
-	}
-	while (1)
-	{
-		printf("$ ");
-		read = getline(&line, &length, stdin);
-		if (read < -1)
-			break;
-		length = strlen(line);
-		if (line[length - 1] == '\n')
-			line[length - 1] = '\0';
-
-		tokenize(line, argvv, bufsize);
 
 
-		pid = fork();
-		if (pid == 0)
-		{
-			if (execve(argvv[0], argvv, NULL) == -1)
-			fprintf(stderr, "Child process could not do execve\n");
-		}
-		else
-		{
-			wait(NULL);
-		}
 
-	
+/**                                                                          
+ * tokenize - parse the input string                                          
+ * @line: string                                                              
+ * @argvv: array of strings to fill                                           
+ * @bufsize: size of buffer for allocation                                    
+ *Return: void                                                                
+ */                                                                           
+void tokenize(char *line, char **argvv, int bufsize)                         
+{                                                                            
+	char *token = NULL;                                                          
+	int     i = 0;                                                               
 
-	}
-	return (0);
-}
+	token = strtok(line, " ");                                                   
+	while (token != NULL)                                                        
+	{                                                                            
+		argvv[i] = token;                                                    
+		token = strtok(NULL, " ");                                           
+		i++;                                                                 
+		if (i >= bufsize)                                                    
+		{                                                                    
+			bufsize += BUFFER_LEN;                                       
+			argvv = realloc(argvv, bufsize * sizeof(char *));            
+			if (!argvv)                                                  
+			{                                                            
+				perror("tokenize fail");                             
+				exit(EXIT_FAILURE);                                  
+			}                                                            
+		}                                                                    
+	}                                                                            
+	argvv[i] = '\0';                                                             
+}                                                                            
+
+/**                                                                          
+ * check_builtin - check if the string passed is a builtin                    
+ * @argvv[0]: string to check                                                 
+ *Return: -1 on falure & >= 1 on success                                      
+ */                                                                           
+int check_builtin(char *argvv)                                               
+{                                                                            
+	int i = 0;                                                                   
+	char *cm[4];                                                                 
+
+	cm[0] = "exit";                                                              
+	cm[1] = "cd";                                                                
+	cm[2] = "help";                                                              
+	cm[3] = "env";                                                               
+	while (i < 4)                                                                
+	{                                                                            
+		if (strcmp(argvv, cm[i]) == 0)                                       
+			return (i + 1);                                                      
+		else                                                                 
+			i++;                                                                 
+	}                                                                            
+	return (-1);                                                                 
+}                                                                            
+
